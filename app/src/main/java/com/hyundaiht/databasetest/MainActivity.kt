8a1f6e3eba65319241ebedf6cc0d887a0a2a1d84
BWooRam <wrbae@hyundaiht.co.kr>
2025-01-06 10:51:13 +0900

    init

---
+++ app/src/main/java/com/hyundaiht/databasetest/MainActivity.kt
@@ -0,0 +1,172 @@
+package com.hyundaiht.databasetest
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hyundaiht.databasetest.ui.ExampleEntity
+import com.hyundaiht.databasetest.ui.MyDatabase
+import com.hyundaiht.databasetest.ui.NewMyDatabase
+import com.hyundaiht.databasetest.ui.theme.DataBaseTestTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    private val tag = javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DataBaseTestTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        TitleAndButton(
+                            title = "DB 생성 테스트",
+                            titleModifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight(),
+                            buttonName = "DB 생성",
+                            buttonModifier = Modifier
+                                .wrapContentSize(),
+                            clickEvent = {
+                                val db = MyDatabase.getInstance(this@MainActivity)
+                                Log.d(tag, "DB 생성 결과 db = $db")
+                                CoroutineScope(Dispatchers.Default).launch {
+                                    runCatching {
+                                        db.exampleDao().allList()
+                                    }.onSuccess {
+                                        Log.d(tag, "DB 생성 테스트 onSuccess data = $it")
+                                    }.onFailure {
+                                        Log.d(tag, "DB 생성 테스트 onFailure error = $it")
+                                    }
+                                }
+                            }
+                        )
+
+                        TitleAndButton(
+                            title = "DB Random insert 테스트",
+                            titleModifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight(),
+                            buttonName = "DB insert 실행",
+                            buttonModifier = Modifier
+                                .wrapContentSize(),
+                            clickEvent = {
+                                val db = MyDatabase.getInstance(this@MainActivity)
+                                CoroutineScope(Dispatchers.Default).launch {
+                                    runCatching {
+                                        db.exampleDao().insert(createRandomEntity())
+                                    }.onSuccess {
+                                        Log.d(tag, "DB Random insert 테스트 onSuccess")
+                                    }.onFailure {
+                                        Log.d(tag, "DB Random insert 테스트 onFailure error = $it")
+                                    }
+                                }
+                            }
+                        )
+
+                        TitleAndButton(
+                            title = "DB 데이터 이전 테스트",
+                            titleModifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight(),
+                            buttonName = "DB 이전 실행",
+                            buttonModifier = Modifier
+                                .wrapContentSize(),
+                            clickEvent = {
+                                val db = NewMyDatabase.getInstance(this@MainActivity)
+                                Log.d(tag, "DB 이전 결과 db = $db")
+                                CoroutineScope(Dispatchers.Default).launch {
+                                    runCatching {
+//                                        db.exampleDao().insert(createRandomEntity())
+//                                        db.exampleDao().insert(createRandomEntity())
+//                                        db.exampleDao().insert(createRandomEntity())
+//                                        db.userDao().insert(createRandomUser())
+//                                        db.userDao().insert(createRandomUser())
+//                                        db.userDao().insert(createRandomUser())
+                                        db.exampleDao().allList()
+                                    }.onSuccess {
+                                        Log.d(tag, "DB 데이터 이전 테스트 onSuccess data = $it")
+                                    }.onFailure {
+                                        Log.d(tag, "DB 데이터 이전 테스트 onFailure error = $it")
+                                    }
+                                }
+                            }
+                        )
+
+                        TitleAndButton(
+                            title = "이전 DB 데이터 삭제 테스트",
+                            titleModifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight(),
+                            buttonName = "이전 DB 삭제 실행",
+                            buttonModifier = Modifier
+                                .wrapContentSize(),
+                            clickEvent = {
+                                val isDelete = deleteDatabase("ASDFASDF.db")
+                                Log.d(tag, "DB 삭제 결과 isDelete = $isDelete")
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun createRandomEntity(): ExampleEntity {
+        val randomId = Random.nextInt(0, 10000)
+        val randomString = randomId.toString()
+        return ExampleEntity(id = randomId, randomString)
+    }
+
+    /*private fun createRandomUser(): UserEntity {
+        val randomId = Random.nextInt(0, 10000)
+        val randomString = randomId.toString()
+        val randomAge = Random.nextInt(0, 99)
+        val randomGender = Random.nextBoolean()
+        return UserEntity(
+            id = randomId,
+            name = randomString,
+            age = randomAge,
+            gender = randomGender
+        )
+    }*/
+}
+
+@Composable
+fun TitleAndButton(
+    title: String,
+    titleModifier: Modifier = Modifier,
+    buttonName: String,
+    buttonModifier: Modifier = Modifier,
+    clickEvent: () -> Unit
+) {
+    Text(
+        text = title,
+        modifier = titleModifier
+    )
+    Button(
+        onClick = clickEvent,
+        modifier = buttonModifier,
+        content = { Text(text = buttonName) }
+    )
+}
---
+++ app/src/main/java/com/hyundaiht/databasetest/ui/LocalDatabase.kt
@@ -0,0 +1,155 @@
+package com.hyundaiht.databasetest.ui
+
+import android.content.Context
+import android.util.Log
+import androidx.room.AutoMigration
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.DeleteColumn
+import androidx.room.DeleteTable
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RenameColumn
+import androidx.room.RenameTable
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
+import androidx.sqlite.db.SupportSQLiteDatabase
+import java.io.File
+import java.io.FileNotFoundException
+
+@Entity(tableName = "example_table")
+data class ExampleEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val data: String,
+    val createdAt: Long = System.currentTimeMillis() // 생성 시간 기록
+)
+
+/*@Entity(tableName = "user2")
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val age: Int,
+    val gender: Boolean
+)*/
+
+@Dao
+interface ExampleDao {
+    @Query("DELETE FROM example_table WHERE :currentTime - createdAt > :expiryTime")
+    suspend fun deleteExpiredData(currentTime: Long, expiryTime: Long)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(exampleEntity: ExampleEntity)
+
+    @Query("SELECT * FROM example_table")
+    suspend fun allList(): List<ExampleEntity>
+}
+
+/*@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(user1Entity: UserEntity)
+
+    @Query("SELECT * FROM user2")
+    suspend fun allList(): List<UserEntity>
+}*/
+
+@Database(entities = [ExampleEntity::class], version = 1)
+abstract class MyDatabase : RoomDatabase() {
+    abstract fun exampleDao(): ExampleDao
+
+    companion object {
+        fun getInstance(context: Context): MyDatabase {
+            return synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext, MyDatabase::class.java, "example_database.db"
+                )
+//                    .createFromFile(getDBfile(context, "example_database.db"))
+//                    .setJournalMode(JournalMode.WRITE_AHEAD_LOGGING)
+//                    .enableMultiInstanceInvalidation()
+//                    .fallbackToDestructiveMigration()
+                    .build()
+                instance
+            }
+        }
+    }
+}
+
+@Database(
+    entities = [
+        ExampleEntity::class,
+//        UserEntity::class,
+    ],
+    version = 5,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2, AutoMigration_1_2::class),
+        AutoMigration(from = 2, to = 3, AutoMigration_2_3::class),
+        AutoMigration(from = 3, to = 4, AutoMigration_3_4::class),
+        AutoMigration(from = 4, to = 5, AutoMigration_4_5::class),
+    ]
+)
+abstract class NewMyDatabase : RoomDatabase() {
+    abstract fun exampleDao(): ExampleDao
+//    abstract fun userDao(): UserDao
+
+    companion object {
+        fun getInstance(context: Context): NewMyDatabase {
+            return synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext, NewMyDatabase::class.java, "example_database.db"
+                )
+//                    .createFromFile(getDBfile(context, "example_database.db"))
+//                    .setJournalMode(JournalMode.WRITE_AHEAD_LOGGING)
+//                    .enableMultiInstanceInvalidation()
+//                    .fallbackToDestructiveMigration()
+                    .build()
+                instance
+            }
+        }
+    }
+}
+
+private fun getDBfile(context: Context, dbName: String): File {
+//            val dbFile = context.getDatabasePath(dbName)
+    val dbFile = File("/data/data/com.hyundaiht.databasetest/files", dbName).apply {
+        setReadable(true)
+        setWritable(true)
+    }
+    if (!dbFile.exists()) {
+        throw FileNotFoundException("Database file not found at: ${dbFile.absolutePath}")
+    }
+    Log.d("MyDatabase", "getDBfile file = $dbFile")
+    return dbFile
+}
+
+
+@RenameTable(fromTableName = "user1", toTableName = "user2")
+class AutoMigration_1_2 : AutoMigrationSpec {
+    override fun onPostMigrate(db: SupportSQLiteDatabase) {
+//        db.execSQL("UPDATE WorkSpec SET `last_enqueue_time` = -1 WHERE `last_enqueue_time` = 0")
+    }
+}
+
+@RenameColumn(tableName = "user2", fromColumnName = "fullName", toColumnName = "name")
+class AutoMigration_2_3 : AutoMigrationSpec {
+    override fun onPostMigrate(db: SupportSQLiteDatabase) {
+//        db.execSQL("UPDATE WorkSpec SET `last_enqueue_time` = -1 WHERE `last_enqueue_time` = 0")
+    }
+}
+
+@DeleteColumn(tableName = "user2", columnName = "middleName")
+class AutoMigration_3_4 : AutoMigrationSpec {
+    override fun onPostMigrate(db: SupportSQLiteDatabase) {
+//        db.execSQL("UPDATE WorkSpec SET `last_enqueue_time` = -1 WHERE `last_enqueue_time` = 0")
+    }
+}
+
+@DeleteTable(tableName = "user2")
+class AutoMigration_4_5 : AutoMigrationSpec {
+    override fun onPostMigrate(db: SupportSQLiteDatabase) {
+//        db.execSQL("UPDATE WorkSpec SET `last_enqueue_time` = -1 WHERE `last_enqueue_time` = 0")
+    }
+}
